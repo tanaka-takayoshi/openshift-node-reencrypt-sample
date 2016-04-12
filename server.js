9dcf690825d956db27ae653ec4d07fcea80741fb
tanaka_733 <tanaka-takayoshi@users.noreply.github.com>
2016-04-12 13:49:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-//var secureServer = https.createServer({
-//   key: fs.readFileSync(path.join(__dirname, 'ssl/server.key')),
-//    cert: fs.readFileSync(path.join(__dirname, 'ssl/server.crt')),
-//    ca: fs.readFileSync(path.join(__dirname, 'ssl/ca.crt')),
-//    requestCert: true,
-//    rejectUnauthorized: false
-//}, app).listen(port, ip, function() {
-//    console.log('Server running on ' + ip + ':' + port);
-//});
-
-var server = http.createServer(app)
-                 .listen(port, ip, function() {
+var secureServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'ssl/server.key')),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl/server.crt')),
+    ca: fs.readFileSync(path.join(__dirname, 'ssl/ca.crt')),
+    requestCert: true,
+    rejectUnauthorized: false
+  }, app).listen(port, ip, function() {
     console.log('Server running on ' + ip + ':' + port);
 });
+
+//var server = http.createServer(app)
+//                 .listen(port, ip, function() {
+//    console.log('Server running on ' + ip + ':' + port);
+//});
